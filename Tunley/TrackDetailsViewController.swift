9d1c8baa13b360abbc8214355614d197178c8f55
Leo Villalobos <villaleobos@gmail.com>
2023-02-09 20:12:42 -0800

    Pass data to TrackDetails view

--- Tunley/TrackDetailsViewController.swift
+++ Tunley/TrackDetailsViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import Nuke
 
 class TrackDetailsViewController: UIViewController {
+    @IBOutlet weak var artworkIView: UIImageView!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
     var track: Track!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.title = track.name
+        navigationItem.title = track.name
+        Nuke.loadImage(with: track.artwork, into: artworkIView)
+        artistLabel.text = track.artist
+        albumLabel.text = track.album
+        genreLabel.text = track.genre
+        releaseDateLabel.text = track.releaseDate.formatted()
+        durationLabel.text = formattedTrackDuration(with: track.durationMillis)
     }
 }
